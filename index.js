99da53e94f5392175a69b8a30763c97aa92610ec
sanchouskin <69318409+sanchouskin@users.noreply.github.com>
2021-10-11 00:52:35 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+var randomNum1=Math.floor(Math.random()*6)+1;
+
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNum1 + ".png");
+var randomNum2=Math.floor(Math.random()*6)+1;
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNum2 + ".png");
