2948498272441cc2fb9b9d4502c8984b60281474
sanchouskin <69318409+sanchouskin@users.noreply.github.com>
2021-10-11 01:10:20 +0600

    Delete Dicee Challenge - Starting Files/Dicee Challenge - Starting Files directory

--- Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/index.js
+++
@@ -1,5 +0,0 @@
-var randomNum1=Math.floor(Math.random()*6)+1;
-
-document.querySelector(".img1").setAttribute("src", "images/dice" + randomNum1 + ".png");
-var randomNum2=Math.floor(Math.random()*6)+1;
-document.querySelector(".img2").setAttribute("src", "images/dice" + randomNum2 + ".png");
